6a3f96518f4960d670adf7e9e5fd81e7dfc8f4d9
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-12-14 22:27:43 +0530

    Added:MySQL cmd and output for creation of table with fields

--- EmpPayrolldb.sql
+++ EmpPayrolldb.sql
@@ -17,3 +17,11 @@ mysql> show databases;
 | world              |
 +--------------------+
 8 rows in set (0.00 sec)
+
+<--UC2 create Table employee_payroll with certain fields-->
+mysql> create Table employee_payroll(
+    -> Id INT AUTO_INCREMENT PRIMARY KEY,
+    -> name varchar(255) NOT NULL,
+    -> salary varchar(255),
+    -> startDate varchar(255));
+Query OK, 0 rows affected (0.12 sec)
