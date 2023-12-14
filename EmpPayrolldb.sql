0dedb590ef1564270d723456c682ad6bc35b0ed9
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-12-14 22:00:35 +0530

    Added: MySQL cmd and output for creation of database

---
+++ EmpPayrolldb.sql
@@ -0,0 +1,19 @@
+<------------Welcome to Employee Payroll Service Database---------------->
+<--UC1 create  Payroll service database-->
+mysql> create database payroll_service;
+Query OK, 1 row affected (0.04 sec)
+
+mysql> show databases;
++--------------------+
+| Database           |
++--------------------+
+| addressbookservice |
+| information_schema |
+| mysql              |
+| payroll_service    |
+| performance_schema |
+| sakila             |
+| sys                |
+| world              |
++--------------------+
+8 rows in set (0.00 sec)
