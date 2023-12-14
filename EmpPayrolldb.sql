0b5621ed226c1ebb0e6add309fe4f3e9cd849d09
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-12-14 22:48:04 +0530

    Added: MySQL cmd and output for populating field value of table

--- EmpPayrolldb.sql
+++ EmpPayrolldb.sql
@@ -25,3 +25,23 @@ mysql> create Table employee_payroll(
     -> salary INT,
     -> startDate varchar(255));
 Query OK, 0 rows affected (0.12 sec)
+
+<--UC3 Populating field values of Table-->
+mysql> insert into employee_payroll(name,salary,startDate)
+    -> values("Vivek",100000,"14/12/2023");
+Query OK, 1 row affected (0.02 sec)
+mysql> insert into employee_payroll(name,salary,startDate)
+    -> values("Ranjan",200000,"15/12/2023");
+Query OK, 1 row affected (0.00 sec)
+mysql> insert into employee_payroll(name,salary,startDate)
+    -> values("Amit",300000,"16/12/2023");
+Query OK, 1 row affected (0.00 sec)
+mysql> select * from employee_payroll;
++----+--------+--------+------------+
+| Id | name   | salary | startDate  |
++----+--------+--------+------------+
+|  1 | Vivek  | 100000 | 14/12/2023 |
+|  2 | Ranjan | 200000 | 15/12/2023 |
+|  3 | Amit   | 300000 | 16/12/2023 |
++----+--------+--------+------------+
+3 rows in set (0.01 sec)
