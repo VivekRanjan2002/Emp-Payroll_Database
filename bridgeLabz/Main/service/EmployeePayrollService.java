2fde0008cca7591cb4b3dd12e7ffffe166b56396
Vivek Ranjan <lit2020047@iiitl.ac.in>
2023-12-18 02:04:36 +0530

    Added: JDBC Update SQL

--- bridgeLabz/Main/service/EmployeePayrollService.java
+++ bridgeLabz/Main/service/EmployeePayrollService.java
@@ -6,16 +6,40 @@ public class EmployeePayrollService {
     public enum IOService{CONSOLE_IO,FILE_IO,DB_IO,REST_IO};
 
     private List<EmployeePayrollData> employeePayrollList;
-    public EmployeePayrollService(){}
+    private  EmployeePayrollDBService EmployeePayrollDBService;
+    // getting singleton instance of EmployeePayrollDBService class
+    public EmployeePayrollService(){
+        EmployeePayrollDBService= EmployeePayrollDBService.getInstance();
+    }
     public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList){
-
+    this();
+    this.employeePayrollList=employeePayrollList;
     }
 
     public List<EmployeePayrollData> readEmployeePayrollData(IOService ioService){
         if(ioService.equals(IOService.DB_IO))
-            this.employeePayrollList= new EmployeePayrollDBService().readData();
+            this.employeePayrollList=  EmployeePayrollDBService.readData();
         return this.employeePayrollList;
     }
+     // update employee salary with name
+    public void updateEmployeeSalary(String name,int salary){
+        int result=  EmployeePayrollDBService.updateEmployeeData(name,salary);
+        if(result==0) return;
+        EmployeePayrollData employeePayrollData= this.getEmployeePayrollData(name);
+        if(employeePayrollData!=null) employeePayrollData.salary=salary;
+    }
+    // check if employee payrolldataList  salary is in sync with DB employee salary
+    public boolean checkEmployeePayrollInSyncWithDB(String name) {
+        List<EmployeePayrollData> employeePayrollDataList= EmployeePayrollDBService.getEmployeePayrollData(name);
+        return employeePayrollDataList.get(0).equals(getEmployeePayrollData(name));
 
+    }
+    // return employeePayrolldata from list with name
+    private EmployeePayrollData getEmployeePayrollData(String name) {
+    return  this.employeePayrollList.stream()
+                .filter(employeePayrollDataItem -> employeePayrollDataItem.name.equals(name) )
+                .findFirst()
+                .orElse(null);
+    }
 
 }
